5acd5680b731c3260f60af25fb13bb2b7ac21798
Andrea Sanchez <andrea.sancheztx@gmail.com>
2020-04-02 18:30:00 -0500

    command changes

--- liri.js
+++ liri.js
@@ -1,2 +1,13 @@
 require('dotenv').config()
 var keys = require("./keys.js");
+// console.log("running liri")
+// console.log(process.argv)
+var command = process.argv[2]
+console.log(command)
+
+
+if (command === "spotify-this-song") {
+    console.log('get songs')
+} else if (command === "movie-this") {
+    console.log("find movie")
+}
